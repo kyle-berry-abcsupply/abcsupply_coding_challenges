cb54181cdf17fb3e766bdfda450038bf17b0e6a7
Kyle Berry <KB066038@abcsupply.com>
2023-03-09 14:23:40 -0600

    Add social media post project

---
+++ C#/SocialMediaPost/PostAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿namespace SocialMediaPost
+{
+    public class PostAnalyzer
+    {
+        private readonly char[] _wordDelimiters = { ' ', '!', '.', ',', '*', '-', '?' };
+        private readonly string _post;
+
+        // TODO: Add your private fields here!
+
+        public PostAnalyzer(string post)
+        {
+            _post = post;
+        }
+
+        public void ProcessPost()
+        {
+            // TODO: Your Code Here!
+        }
+
+        // Returns the total number of distinct words
+        public int GetDistinctWordCount()
+        {
+            return 0; // TODO: Your Code Here!
+        }
+
+        // Determine how many times a word appears in the post, given a word
+        public int GetIndividualWordCount(string word)
+        {
+            return 0; // TODO: Your Code Here!
+        }
+    }
+}
